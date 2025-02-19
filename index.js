dc15e108b1442ce3d32bbbbf112281d2f67e735d
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2025-02-19 10:31:51 +0000

    ejs and new css setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hiii")
+const express=require("express");
+
+const app=express()
+app.set("view engine","ejs")
+app.use(express.static("public")) //static files
+
+app.get("/",(req,res)=>{
+    res.render("homepage",{title:"e28886"})
+
+})
+
+app.listen(3000,()=>{
+    console.log("server fired up")
+})
